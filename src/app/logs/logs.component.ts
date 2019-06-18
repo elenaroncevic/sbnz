b0a7a3857d4f5d0cb8bcc5b17bb7a3c511c28943
elenaroncevic <roncevicelena@gmail.com>
2019-06-18 13:45:17 +0200

    pretraga

--- src/app/logs/logs.component.ts
+++ src/app/logs/logs.component.ts
@@ -11,17 +11,37 @@ export class LogsComponent implements OnInit {
 
 
   logs: Log[];
+  database : boolean = true;
+
+  regex : boolean = false;
+  log: Log ;
+
+  regexText: string = null;
+
+  logTypes = ["Login","VirusThreat", "ThreatEliminated", "Information"];
+  logStatus = ["Ok", "Warning", "Error"];
  // loading : true;
  
   constructor(private logService: LogService) { }
  
   ngOnInit() {
     this.getLogs();
+    this.log = {type:null, status:null, text: null, time:null, id:null, machineId:null, userUsername: null};
+    console.log(this.log);
   }
  
   getLogs(): void {
     this.logs = this.logService.getLogs();
    /*  .subscribe(data =>{ 
+        this.logs = data
+        this.loading = false;
+      }); */
+  }
+
+  search():void{
+    
+    this.logs = this.logService.search(this.database, this.regexText, this.log);
+    /*  .subscribe(data =>{ 
         this.logs = data
         this.loading = false;
       }); */
